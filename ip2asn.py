5a874d3a5ce0a32c6f7a72f3ff76585ef0e5bbef
Wes Hardaker <opensource@hardakers.net>
2020-04-23 05:38:27 -0700

    initial import version to start packaging

---
+++ ip2asn.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+
+"""Reads an ip2asn file into memory, and uses the data to extract
+information about an address.
+
+Usage:
+    Prep:
+
+    curl -o ip2asn-v4-u32.tsv.gz https://iptoasn.com/data/ip2asn-v4-u32.tsv.gz
+    gunzip ip2asn-v4-u32.tsv.gz
+    pip3 install ip2asn
+
+    Code:
+
+    import ip2asn
+    i2a = ip2asn.IP2ASN("ip2asn-v4-u32.tsv")
+    result = i2a.lookup_address("93.184.216.34")
+"""
+
+import pyfsdb
+import ipaddress
+from bisect import bisect
+
+class IP2ASN():
+    """A container for accessing data within an ip2asn file"""
+    def __init__(self, ip2asn_file, ipversion=4):
+        self._file = ip2asn_file
+        self._version = ipversion
+
+        self.read_data()
+
+    def read_data(self):
+        """Read data from the ip2asn file"""
+        iptoasn = pyfsdb.Fsdb(self._file)
+        iptoasn.column_names = ['start','end','ASN', 'country','name']
+
+        (self._start_col, self._end_col,
+         self._asn_col, self._country_col,
+         self._name_col) = iptoasn.get_column_numbers(iptoasn.column_names)
+
+        # XXX: fsdb should do this for us
+        self._data = []
+        self._left_keys = []
+        for row in iptoasn:
+            row[self._start_col] = int(row[self._start_col])
+            row[self._end_col] = int(row[self._end_col])
+            self._data.append(row)
+            self._left_keys.append(int(row[self._start_col]))
+
+    def lookup_address_row(self, address):
+        """Look up an ip address from the ip2asn data, and return its row."""
+        # get a numeric representation
+        ip = int(ipaddress.IPv4Address(address))
+
+        point = bisect(self._left_keys, ip)
+        if point != len(self._left_keys):
+            row = self._data[point-1]
+            if ip >= row[self._start_col] and ip <= row[self._end_col]:
+                return row
+            
+    def lookup_address(self, address):
+        """Look up an ip address (dotted string) and return a 
+        dictionary of information about it.
+        (transforming the row returned by lookup_address_row)"""
+        results = self.lookup_address_row(address)
+        ip = int(ipaddress.IPv4Address(address))
+        return {
+            'ip_text': address,
+            'ip_numeric': ip,
+            'ip_range': [results[self._start_col], results[self._end_col]],
+            'ASN': results[self._asn_col],
+            'country': results[self._country_col],
+            'owner': results[self._name_col],
+        }
+
+def testmain():
+    import os
+    t = IP2ASN(os.environ['HOME'] + "/lib/ip2asn-v4-u32.tsv")
+    print(t.lookup_address("8.8.8.8"))
+
+if __name__ == "__main__":
+    testmain()
+
