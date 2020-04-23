35feea6c8176507da309472e2fcbd76f28ceeeea
Wes Hardaker <opensource@hardakers.net>
2020-04-23 10:22:40 -0700

    cleanup

--- ip2asn/main.py
+++ ip2asn/main.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 
+"""Converts a bunch of addresses to displayed details about them"""
+
 import argparse
 import sys
 import os
 import ip2asn
 
 def parse_args():
+    """Handles argument parsing for the ip2asn script."""
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
                                      description="Describes one or more IP addresses from an ip2asn database",
-                                     epilog="""Example Usage: 
-                                     ip2asn -f ip2asn-v4-43.tsv 1.1.1.1""")
+                                     epilog="""Example Usage: ip2asn -f ip2asn-v4-43.tsv 1.1.1.1""")
 
     parser.add_argument("-f", "--ip2asn-database", type=str,
                         default=os.environ['HOME'] + "/lib/ip2asn-v4-u32.tsv",
@@ -21,7 +23,18 @@ def parse_args():
     args = parser.parse_args()
     return args
 
+def print_result(result):
+    """Displays the results to the output terminal/stdout"""
+    print("Address: {}".format(address))
+    print("  Numeric ip: {}".format(result['ip_numeric']))
+    print("         ASN: {}".format(result['ASN']))
+    print("       Owner: {}".format(result['owner']))
+    print("     Country: {}".format(result['country']))
+    print("    ip_range: {}".format(result['ip_range']))
+    print("")
+
 def main():
+    "The meat of the ip2asn script"
     args = parse_args()
 
     i2a = ip2asn.IP2ASN(args.ip2asn_database, ipversion=None)
@@ -31,13 +44,6 @@ def main():
             print("ERROR: address '%s' was not found in the database".format(address))
             continue
         
-        print("Address: {}".format(address))
-        print("  Numeric ip: {}".format(result['ip_numeric']))
-        print("         ASN: {}".format(result['ASN']))
-        print("       Owner: {}".format(result['owner']))
-        print("     Country: {}".format(result['country']))
-        print("    ip_range: {}".format(result['ip_range']))
-        print("")
 
 if __name__ == "__main__":
     main()
