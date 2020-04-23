efbcb5558a23fa440fa13d33782a003624089f55
Wes Hardaker <opensource@hardakers.net>
2020-04-23 08:48:12 -0700

    support looking at v6

--- ip2asn/tests/test_ip2asn.py
+++ ip2asn/tests/test_ip2asn.py
@@ -51,6 +51,29 @@ class test_ip2asn(unittest.TestCase):
 1.1.21.0	1.1.63.255	0	None	Not routed
 """)
 
+    def get_first_20_rows_v6(self):
+        """return the first 20 rows of a v6 database sample"""
+        return io.StringIO("""::	::1	0	None	Not routed
+64:ff9b::1:0:0	100::ffff:ffff:ffff:ffff	0	None	Not routed
+100:0:0:1::	2001:0:ffff:ffff:ffff:ffff:ffff:ffff	0	None	Not routed
+2001:1::	2001:4:111:ffff:ffff:ffff:ffff:ffff	0	None	Not routed
+2001:4:112::	2001:4:112:ffff:ffff:ffff:ffff:ffff	112	US	ROOTSERV - DNS-OARC
+2001:4:113::	2001:c0:2:ffff:ffff:ffff:ffff:ffff	0	None	Not routed
+2001:c0:3::	2001:c0:3:ffff:ffff:ffff:ffff:ffff	22884	MX	TOTAL PLAY TELECOMUNICACIONES SA DE CV
+2001:c0:4::	2001:1ff:ffff:ffff:ffff:ffff:ffff:ffff	0	None	Not routed
+2001:200::	2001:200:5ff:ffff:ffff:ffff:ffff:ffff	2500	JP	WIDE-BB WIDE Project
+2001:200:600::	2001:200:6ff:ffff:ffff:ffff:ffff:ffff	7667	JP	KDDLAB KDDI R&D Laboratories, INC.
+2001:200:700::	2001:200:8ff:ffff:ffff:ffff:ffff:ffff	2500	JP	WIDE-BB WIDE Project
+2001:200:900::	2001:200:9ff:ffff:ffff:ffff:ffff:ffff	7660	JP	APAN-JP Asia Pacific Advanced Network - Japan
+2001:200:a00::	2001:200:bfff:ffff:ffff:ffff:ffff:ffff	2500	JP	WIDE-BB WIDE Project
+2001:200:c000::	2001:200:dfff:ffff:ffff:ffff:ffff:ffff	23634	JP	E-DNS-JP WIDE Project
+2001:200:e000::	2001:200:ffff:ffff:ffff:ffff:ffff:ffff	7660	JP	APAN-JP Asia Pacific Advanced Network - Japan
+2001:201::	2001:217:ffff:ffff:ffff:ffff:ffff:ffff	0	None	Not routed
+2001:218::	2001:218:21ff:ffff:ffff:ffff:ffff:ffff	2914	US	NTT-COMMUNICATIONS-2914 - NTT America, Inc.
+2001:218:2200::	2001:218:22ff:ffff:ffff:ffff:ffff:ffff	18259	JP	HIGE NTT Communications Corporation
+2001:218:2300::	2001:218:3003:ffff:ffff:ffff:ffff:ffff	2914	US	NTT-COMMUNICATIONS-2914 - NTT America, Inc.
+2001:218:3004::	2001:218:3004:ffff:ffff:ffff:ffff:ffff	20940	EU	AKAMAI-ASN1""")
+
     def test_load(self):
         import ip2asn
         self.assertTrue(ip2asn, "Successfully loaded ip2asn")
@@ -72,7 +95,7 @@ class test_ip2asn(unittest.TestCase):
         i2a = ip2asn.IP2ASN(self.get_first_20_rows_v4_ints())
 
         result = i2a.lookup_address("1.1.1.2")
-        self.assertTrue(isinstance(result, dict), "properly returned a list")
+        self.assertTrue(isinstance(result, dict), "properly returned a dict")
         self.assertEqual(result,
                          {'ip_text': '1.1.1.2',
                           'ip_numeric': 16843010,
@@ -87,7 +110,7 @@ class test_ip2asn(unittest.TestCase):
         i2a = ip2asn.IP2ASN(self.get_first_20_rows_v4_ascii())
 
         result = i2a.lookup_address("1.1.1.2")
-        self.assertTrue(isinstance(result, dict), "properly returned a list")
+        self.assertTrue(isinstance(result, dict), "properly returned a dict")
         self.assertEqual(result,
                          {'ip_text': '1.1.1.2',
                           'ip_numeric': 16843010,
@@ -96,4 +119,21 @@ class test_ip2asn(unittest.TestCase):
                           'country': 'US',
                           'owner': 'CLOUDFLARENET - Cloudflare, Inc.'},
                          "Data returned matched expected")
+        
+    def test_lookup_with_ipv6(self):
+        import ip2asn
+        i2a = ip2asn.IP2ASN(self.get_first_20_rows_v6(), ipversion=6)
+
+        result = i2a.lookup_address("2001:200::42")
+        self.assertTrue(isinstance(result, dict), "properly returned a dict")
+        print(result)
+        self.assertEqual(result,
+                         {'ip_text': '2001:200::42',
+                          'ip_numeric': 42540528726795050063891204319802818626,
+                          'ip_range': [42540528726795050063891204319802818560,
+                                       42540528728651960122819274732151504895],
+                          'ASN': '2500',
+                          'country': 'JP',
+                          'owner': 'WIDE-BB WIDE Project'},
+                         "v6 Data returned matched expected")
         
