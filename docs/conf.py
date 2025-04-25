377a095d0dd54325a90e9b69c0ae11f877905b96
Wes Hardaker <opensource@hardakers.net>
2025-04-25 16:11:10 -0700

    version 1.6.3:

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'ip2asn'
 copyright = '2020-2025, Wes Hardaker'
 author = 'Wes Hardaker'
-release = '1.6.2'
+release = '1.6.3'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
